c007d12338163bb5a0ce94608438d31b98c54ebf
Monish Bashar Nawaz <monishnawaz02@gmail.com>
2026-07-29 23:27:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (56.53%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        vector<int> ans;
+
+        int n = mat.size();
+        int m = mat[0].size();
+
+        int top = 0, bottom = n - 1;
+        int left = 0, right = m - 1;
+
+        while (top <= bottom && left <= right) {
+
+            // Traverse left -> right
+            for (int i = left; i <= right; i++) {
+                ans.push_back(mat[top][i]);
+            }
+            top++;
+
+            // Traverse top -> bottom
+            for (int i = top; i <= bottom; i++) {
+                ans.push_back(mat[i][right]);
+            }
+            right--;
+
+            // Traverse right -> left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.push_back(mat[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // Traverse bottom -> top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.push_back(mat[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return ans;
+    }
+};
